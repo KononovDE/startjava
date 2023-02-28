e5c796681e4fda91a7b5038766fb2b40bc611817
KononovDE <118136566+KononovDE@users.noreply.github.com>
2023-02-28 19:20:02 +0200

    Update Player.java

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,13 +1,29 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
+    private int[] nums = new int[10];
+    private int currentTry;
 
     public Player(String name) {
         this.name = name;
+        Arrays.fill(nums, 0);
     }
-
     public String getName() {
         return name;
+    }
+    public int[] getNums() {
+        return nums;
+    }
+    public void setNums(int[] nums) {
+        this.nums = nums;
+    }
+    public int getCurrentTry() {
+        return currentTry;
+    }
+    public void setCurrentTry(int currentTry) {
+        this.currentTry = currentTry;
     }
 }
