8b1a718dad54d465ad4fdecaf011f7bab1f8af3f
KononovDE <118136566+KononovDE@users.noreply.github.com>
2023-03-15 06:05:15 +0200

    Update Player.java

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -7,51 +7,49 @@ import java.util.Scanner;
 public class Player {
     private String name;
     private int[] nums = new int[10];
-    private int currentTry = 1;
-    private int currentNum;
-    private int wins = 0;
+    private int currentTry;
+    private int wins;
 
     public Player(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
     }
 
-    public int[] getNums() {
-        return Arrays.copyOf(nums, currentTry - 1);
-    }
-
-    public void addNum(int num) {
-        setCurrentNum(num);
-        nums[currentTry - 1] = currentNum;
-        currentTry = currentTry + 1;
-    }
-
     public int getCurrentTry() {
-        return currentTry;
-    }
-
-    public void clearTries() {
-        Arrays.fill(nums, 0, currentTry - 1, 0);
-        this.currentTry = 1;
-    }
-
-    public void setCurrentNum(int num) {
-        Scanner scanner =  new Scanner(System.in);
-        while (num <= 0 || num > 100) {
-            System.out.println("Число " + num + " не входит в полуинтервал (0, 100]");
-            num = scanner.nextInt();
-        }
-        currentNum = num;
+        return currentTry + 1;
     }
 
     public int getCurrentNum() {
-        return currentNum;
+        return nums[currentTry];
     }
 
-    public void addWin() {
+    public void addNum(int num) {
+        Scanner scanner =  new Scanner(System.in);
+        if (num <= 0 || num > 100) {
+            System.out.println("Число " + num + " не входит в полуинтервал (0, 100]");
+        } else {
+            nums[currentTry] = num;
+        }
+    }
+
+    public void incrementWin() {
         wins++;
+    }
+
+    public void incrementCurrentTry() {
+        currentTry++;
+    }
+
+    public int[] getNums() {
+        return Arrays.copyOf(nums, currentTry);
+    }
+
+    public void clearTries() {
+        Arrays.fill(nums, 0, currentTry, 0);
+        currentTry = 0;
     }
 
     public int getWins() {
