5c2dde13f0c2ecd8ac6c82edf4411785bf41f6a3
KononovDE <118136566+KononovDE@users.noreply.github.com>
2023-04-10 04:48:45 +0200

    Update Calculator.java

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -5,13 +5,6 @@ public class Calculator {
     private double num2;
     private char sign;
 
-    public void split(String expression) {
-        String[] partsExpression = expression.split(" ");
-        num1 = Integer.parseInt(partsExpression[0]);
-        sign = partsExpression[1].charAt(0);
-        num2 = Integer.parseInt(partsExpression[2]);
-    }
-
     public double calculate(String expression) {
         split(expression);
         switch (sign) {
@@ -31,5 +24,12 @@ public class Calculator {
                 System.out.println("Математическая операция " + sign + " не поддерживается");
         }
         return 0;
+    }
+
+    private void split(String expression) {
+        String[] partsExpression = expression.split(" ");
+        num1 = Integer.parseInt(partsExpression[0]);
+        sign = partsExpression[1].charAt(0);
+        num2 = Integer.parseInt(partsExpression[2]);
     }
 }
