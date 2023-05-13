673dd0be659be661237fe1e04589d50b9dee7163
KononovDE <118136566+KononovDE@users.noreply.github.com>
2023-05-13 14:45:10 +0200

    Update create_db.sql

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -5,21 +5,20 @@ CREATE DATABASE Jaeger;
 DROP TABLE IF EXISTS Jaeger;
 
 CREATE TABLE Jaeger (
-	id SERIAL PRIMARY KEY, 
-	modelName TEXT,
-	mark      TEXT,
-	height    INTEGER,
-              CONSTRAINT heightNotNegative
-              CHECK(height > 0),
-	weight    INTEGER,
-              CONSTRAINT weightNotNegative
-              CHECK(weight > 0),
-	status    TEXT,
-	origin    TEXT,
-	launch    DATE,
-	kaijuKill INTEGER
+	id	SERIAL	PRIMARY KEY, 
+	modelName	TEXT,
+	mark		TEXT,
+	height		INTEGER,
+			CONSTRAINT heightNotNegative
+			CHECK(height > 0),
+	weight		INTEGER,
+			CONSTRAINT weightNotNegative
+			CHECK(weight > 0),
+	status		TEXT,
+	origin		TEXT,
+	launch		DATE,
+	kaijuKill	INTEGER
 );
 
 \i 'init_db.sql'
-
 \i 'queries.sql'
